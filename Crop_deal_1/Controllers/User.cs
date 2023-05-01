aeece910a1e2470e98298ab295e3d74fc562b654
HP <HP@LAPTOP-8461GSRU>
2023-05-01 23:26:31 +0530

    vedant to commit1

--- Crop_deal_1/Controllers/User.cs
+++ Crop_deal_1/Controllers/User.cs
@@ -1,5 +1,6 @@
 ﻿using Crop_deal_1.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 
 namespace Crop_deal_1.Controllers
 {
@@ -14,10 +15,129 @@ namespace Crop_deal_1.Controllers
             this.context = context;  
         }
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> GetAllUsers()
         {
-            int Delaman = 0;
-            return View();
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Name/{name}")]
+        public async Task<IActionResult> GetUserByName(string name)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.User_name == name);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Contact/{contact}")]
+        public async Task<IActionResult> GetUserByContact(string contact)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.User_contact == contact);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Role/{role}")]
+        public async Task<IActionResult> GetUserByRole(string role)
+        {
+            var user = await context.Users.Where(u => u.User_roles.ToLower() == role.ToLower()).ToListAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Email/{email}")]
+        public async Task<IActionResult> GetUserByEmail(string email)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Email_id == email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("IsActive/{isActive}")]
+        public async Task<IActionResult> GetUserByIsActive(bool isActive)
+        {
+            var user = await context.Users.Where(u => u.Is_Active == isActive).ToListAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Subscribe/{isSubscribe}")]
+        public async Task<IActionResult> GetUserBySubscribe(bool isSubscribe)
+        {
+            var user = await context.Users.Where(u => u.Is_subscribe == isSubscribe).ToListAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Bank/{bankName}")]
+        public async Task<IActionResult> GetUserByBankName(string bankName)
+        {
+            var user = await context.Users.Where(u => u.Bank_name.ToLower() == bankName.ToLower()).ToListAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("Account/{accountNo}")]
+        public async Task<IActionResult> GetUserByBankAccountNo(string accountNo)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Bank_account_no == accountNo);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+
     }
 }
---
+++ Crop_deal_1/Migrations/20230501165815_CreateCropDealDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crop_deal_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCropDealDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
